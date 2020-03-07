0473d160ba0619409b26a450b661c1a1d4b150ae
Akhilesh.Prathuri <akhilesh.prathuri@servicenow.com>
2020-03-07 23:58:01 +0530

    Strings medium problem 3

---
+++ src/Strings/Medium/LongestSubStringWithOutRepetition_3.java
@@ -0,0 +1,34 @@
+package Strings.Medium;
+
+import java.util.HashSet;
+
+public class LongestSubStringWithOutRepetition_3 {
+
+	public int lengthOfLongestSubstring(String s) {
+          int result = 0;
+          // sliding window technique
+          HashSet<Character> container = new HashSet<>();
+          int slidingWindowStart = 0;
+          int slidingWindowEnd = 0;
+          
+          while(slidingWindowEnd < s.length()) {
+        	     if(!container.contains(s.charAt(slidingWindowEnd))) {
+        	    	     container.add(s.charAt(slidingWindowEnd));
+        	    	     slidingWindowEnd++;
+        	    	     result = Math.max(container.size(), result);
+
+        	     }else {
+        	       	 container.remove(s.charAt(slidingWindowStart));
+        	      	 slidingWindowStart++;
+        	      	 
+        	     }
+          }
+          
+          return result;
+	}
+
+	public static void main(String[] args) {
+          int result = new LongestSubStringWithOutRepetition_3().lengthOfLongestSubstring("abcdee");
+          System.out.print(result);
+	}
+}
