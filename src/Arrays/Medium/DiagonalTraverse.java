9d8edc9f8b985bedab7fdd69e63e557b88fc8e8f
Akhilesh.Prathuri <akhilesh.prathuri@servicenow.com>
2020-05-01 22:38:56 +0530

    Array 2D

--- src/Arrays/Medium/DiagonalTraverse.java
+++ src/Arrays/Medium/DiagonalTraverse.java
@@ -3,6 +3,7 @@ package Arrays.Medium;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /*
  * 498. Diagonal Traverse
@@ -18,56 +19,83 @@ Output:  [1,2,4,7,5,3,6,8,9]
  */
 public class DiagonalTraverse {
 
+	int count = 0;
 	public static void main(String[] args) {
 		 int[] ans = new DiagonalTraverse().findDiagonalOrder(new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+		 int[] ans1 = new DiagonalTraverse().findDiagonalOrder(new int[][] {{7}, {9}, {6}});
 
 	}
 
-	public static int[] findDiagonalOrder(int[][] matrix) {
-		  // Variables to track the size of the matrix
-        int rowLength = matrix.length;
-        int colLength = matrix[0].length;
-        
-        // The two arrays as explained in the algorithm
-        int[] result = new int[rowLength*colLength];
-        int k = 0;
-        ArrayList<Integer> intermediate = new ArrayList<Integer>();
-        
-        // We have to go over all the elements in the first
-        // row and the last column to cover all possible diagonals
-        for (int d = 0; d < rowLength + colLength - 1; d++) {
-            
-            // Clear the intermediate array every time we start
-            // to process another diagonal
-            intermediate.clear();
-            
-            // We need to figure out the "head" of this diagonal
-            // The elements in the first row and the last column
-            // are the respective heads.
-            int r = d < colLength ? 0 : d - colLength + 1;
-            int c = d < colLength ? d : colLength - 1;
-            
-            // Iterate until one of the indices goes out of scope
-            // Take note of the index math to go down the diagonal
-            while (r < rowLength && c > -1) {
-                
-                intermediate.add(matrix[r][c]);
-                ++r;
-                --c;
-            }
-                
-            // Reverse even numbered diagonals. The
-            // article says we have to reverse odd 
-            // numbered articles but here, the numbering
-            // is starting from 0 :P
-            if (d % 2 == 0) {
-                Collections.reverse(intermediate);
-            }
-            
-            for (int i = 0; i < intermediate.size(); i++) {
-                result[k++] = intermediate.get(i);
-            }
-        }
-        return result;
+	public int[] findDiagonalOrder(int[][] matrix) {
+		
+		if(matrix == null || matrix.length == 0)
+			return new int[0];
+		
+		int rowLength =  matrix.length;
+		int colLength = matrix[0].length;
+		int[] res = new int[rowLength * colLength];
+		int k = 0;
+		ArrayList<Integer> result1 =  processFirstRow(matrix);
+		for(int a : result1)
+			res[k++] = a;
+		ArrayList<Integer> result2 =  processLastColumn(matrix);
+		for(int a : result2)
+			res[k++] = a;
+		
+		return res;
+	}
+	
+	private ArrayList<Integer> processFirstRow(int[][] matrix) {
+		int colLength = matrix[0].length;
+		int row=0, col=0;
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		ArrayList<Integer> tmpArray = new ArrayList<Integer>();
+		for(int i=0; i< colLength; i++) {
+			col = i;
+			while(true) {
+				if(row > matrix.length -1 || col < 0) {
+					row = 0;
+					break;
+				}
+				tmpArray.add(matrix[row][col]);	
+				row ++;
+				col --;
+			}
+			if(count % 2 == 0) {
+				Collections.reverse(tmpArray);
+			}
+			count++;
+			result.addAll(tmpArray);
+			tmpArray.clear();
+		}
+		return result;
+		 
+	}
+	
+	private ArrayList<Integer> processLastColumn(int[][] matrix) {
+		int rowLength = matrix.length;
+		int row=1, col= matrix[0].length - 1;
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		ArrayList<Integer> tmpArray = new ArrayList<Integer>();
+		for(int i= 1; i< rowLength; i++) {
+			row = i;
+			while(true) {
+				if(row > matrix.length -1 || col < 0 || col > matrix[0].length -1 ) {
+					row = 0;
+					col = matrix[0].length - 1;
+					break;
+				}
+				tmpArray.add(matrix[row][col]);	
+				row ++;
+				col --;
+			}
+			if(count % 2 == 0) {
+				Collections.reverse(tmpArray);
+			}
+			count++;
+			result.addAll(tmpArray);
+			tmpArray.clear();
+		}
+		return result;
 	}
 }
